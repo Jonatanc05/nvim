4d8823e1550d4e007b550883fa5e20c7d8b8eed2
Jônata <Jonatanc0511@gmail.com>
2022-04-04 10:48:05 -0300

    Forget <c-k> for signature help because it overrides window resize

--- init.lua
+++ init.lua
@@ -98,7 +98,6 @@ vim.api.nvim_set_keymap("i", "<C-BS>",    "<C-W>", {})
 
 -- LSP mappings
 vim.api.nvim_set_keymap("n", "<K>",       "<cmd>lua vim.lsp.buf.hover()<CR>", {})
-vim.api.nvim_set_keymap("n", "<C-k>",     "<cmd>lua vim.lsp.buf.signature_help()<CR>", {})
 vim.api.nvim_set_keymap("n", "<leader>r", "<cmd>lua vim.lsp.buf.rename()<CR>", {})
 vim.api.nvim_set_keymap("n", "gD",        "<cmd>lua vim.lsp.buf.declaration()<CR>", {})
 vim.api.nvim_set_keymap("n", "gd",        "<cmd>lua vim.lsp.buf.definition()<CR>", {})
